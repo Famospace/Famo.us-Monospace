35e514624952aa4f239d3607be415ca174047b43
Amar Patel <theamarpatel@gmail.com>
2014-06-13 23:22:52 -0700

    cube moves but not only xy axis

--- app/src/views/RotatingCube.js
+++ app/src/views/RotatingCube.js
@@ -6,6 +6,7 @@ define(function(require, exports, module) {
     var StateModifier  = require('famous/modifiers/StateModifier');
     var MouseSync      = require('famous/inputs/MouseSync');
     var Transitionable = require('famous/transitions/Transitionable');
+    var Timer          = require('famous/utilities/Timer');
 
     var CubeView = require('views/CubeView');
 
@@ -17,18 +18,17 @@ define(function(require, exports, module) {
             size: [undefined, undefined]
         });
 
-        var sync = new MouseSync();
+        var largeCubeSync = new MouseSync();
+        var smallCubeSync = new MouseSync();
 
-        backgroundSurface.pipe(sync);
+        backgroundSurface.pipe(largeCubeSync);
 
         this.add(backgroundSurface);
 
         var position = [0, 0];
+        var smallPosition = [150, 150, 150];
 
-        sync.on('update', function (data) {
-            position[0] += data.delta[0];
-            position[1] -= data.delta[1];
-        });
+
 
         var rotateModifier = new Modifier({
             transform: function () {
@@ -42,14 +42,44 @@ define(function(require, exports, module) {
         var smallCube = new CubeView({ size: 50 });
 
         var smallCubeModifier = new Modifier({
-            transform: Transform.translate(150, 150, 150)
+            transform: function () {
+                return Transform.translate(smallPosition[0], smallPosition[1], smallPosition[2]);
+            }
         });
 
-        smallCube.pipe(sync);
+        for (var i=0;i<smallCube.surfaces.length;i++) {
+            smallCube.surfaces[i].pipe(smallCubeSync);
+            // smallCube.surfaces[i].on('mousedown', function (data) {
+
+            // }.bind(this));
+        }
+
+
+        smallCubeSync.on('update', function (data) {
+            var x = Math.abs(data.delta[0]);
+            var y = Math.abs(data.delta[1]);
+            if (x > y) {
+                smallPosition[0] += data.delta[0];
+            } else {
+                smallPosition[1] += data.delta[1];
+            }
+        }.bind(this));
+
         
         node.add(smallCubeModifier).add(smallCube);
 
+
+
+        largeCubeSync.on('update', function (data) {
+            position[0] += data.delta[0];
+            position[1] -= data.delta[1];
+        });
+
         var cube = new CubeView();
+
+        for (var j=0;j<cube.surfaces.length;j++) {
+            cube.surfaces[j].setProperties({pointerEvents: 'none'});
+        }
 
         node.add(cube);
 

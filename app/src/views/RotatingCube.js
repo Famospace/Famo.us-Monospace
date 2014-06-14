dbaa1923bf63d5520f9ff16dc95b50c16ec1c928
Amar Patel <theamarpatel@gmail.com>
2014-06-14 15:10:48 -0700

    cube moves only x or y

--- app/src/views/RotatingCube.js
+++ app/src/views/RotatingCube.js
@@ -6,7 +6,7 @@ define(function(require, exports, module) {
     var StateModifier  = require('famous/modifiers/StateModifier');
     var MouseSync      = require('famous/inputs/MouseSync');
     var Transitionable = require('famous/transitions/Transitionable');
-    var Timer          = require('famous/utilities/Timer');
+    var Draggable      = require('famous/modifiers/Draggable');
 
     var CubeView = require('views/CubeView');
 
@@ -48,22 +48,88 @@ define(function(require, exports, module) {
         });
 
         for (var i=0;i<smallCube.surfaces.length;i++) {
-            smallCube.surfaces[i].pipe(smallCubeSync);
-            // smallCube.surfaces[i].on('mousedown', function (data) {
-
-            // }.bind(this));
+            // smallCube.surfaces[i].pipe(smallCubeSync);
+            _setMovementListeners(smallCube.surfaces[i]);
         }
 
+        function _setMovementListeners (surface) {
+            var downData, upData;
+            surface.on('mousedown', function (data) {
+                downData = data;
+            });
+            surface.on('mouseup', function (data) {
+                upData = data;
+                _moveSmallCube(downData, upData);
+            });
+        }
 
-        smallCubeSync.on('update', function (data) {
-            var x = Math.abs(data.delta[0]);
-            var y = Math.abs(data.delta[1]);
-            if (x > y) {
-                smallPosition[0] += data.delta[0];
-            } else {
-                smallPosition[1] += data.delta[1];
+        function _moveSmallCube (downData, upData) {
+            var xDelta = Math.abs(downData.x - upData.x);
+            var yDelta = Math.abs(downData.y - upData.y);
+            if (xDelta > 10 || yDelta > 10) {
+                // vertical
+                if (yDelta > xDelta) {
+                    // delta is positive
+                    if (downData.y - upData.y > 0) {
+                        smallPosition[1] -= 100;                           
+                    } else {
+                    // delta is negative
+                        smallPosition[1] += 100;                           
+                    }
+                // horizontal
+                } else {
+                    // delta is positive
+                    if (downData.x - upData.x > 0) {
+                        smallPosition[0] -= 100;                           
+                    } else {
+                    // delta is negative
+                        smallPosition[0] += 100;                           
+                    }
+                }
             }
-        }.bind(this));
+        }
+
+        /*
+        Objective:
+            assess trend of mouse-movement
+                if (xDelta > yDelta)
+                    isolate x-axis (aka prohibit y-axis translations)
+                else
+                    isolate y-axis (aka prohibit x-axis translations)
+
+            What if:
+                _____________
+                |///////////|
+                |//         |
+                |//         |
+                |//         |
+                |//_________|
+
+                when '/' area is reached (assuming start pointer coords are 
+                    in center of cube), the cube starts *only* moving in that
+                    direction
+                  process
+                    mousedown coords inside surface
+                    move pointer to 'edge' of surface
+                        direction is locked
+                            mouse must still be down
+                                maybe calculate slope to determine x/y axis
+
+            if mouse approaches surface edge area:
+                based on edge, cube moves in that direction by 1
+        */
+
+
+        // smallCubeSync.on('update', function (data) {
+        //     var x = Math.abs(data.delta[0]);
+        //     var y = Math.abs(data.delta[1]);
+        //     console.log(data);
+        //     if (x > y) {
+        //         smallPosition[0] += data.delta[0];
+        //     } else {
+        //         smallPosition[1] += data.delta[1];
+        //     }
+        // }.bind(this));
 
         
         node.add(smallCubeModifier).add(smallCube);

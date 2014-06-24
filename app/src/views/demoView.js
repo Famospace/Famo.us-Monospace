7fc678e3ce8ece934e75924f123546e826dbf0af
Amar Patel <theamarpatel@gmail.com>
2014-06-23 22:28:24 -0700

    changed zindex of text

--- app/src/views/demoView.js
+++ app/src/views/demoView.js
@@ -162,7 +162,8 @@ define(function(require, exports, module) {
         properties: {
           textAlign: 'center',
           fontSize: '2rem',
-          fontFamily: "HelveticaNeue-Light"
+          fontFamily: "HelveticaNeue-Light",
+          zIndex: 3
         }
       });
 
@@ -191,7 +192,8 @@ define(function(require, exports, module) {
         properties: {
           textAlign: 'center',
           fontSize: '2rem',
-          fontFamily: "HelveticaNeue-Light"
+          fontFamily: "HelveticaNeue-Light",
+          zIndex: 3
         }
       });
 
@@ -220,7 +222,8 @@ define(function(require, exports, module) {
         properties: {
           textAlign: 'center',
           fontSize: '2rem',
-          fontFamily: "HelveticaNeue-Light"
+          fontFamily: "HelveticaNeue-Light",
+          zIndex: 3
         }
       });
 
@@ -249,7 +252,8 @@ define(function(require, exports, module) {
         properties: {
           textAlign: 'center',
           fontSize: '2rem',
-          fontFamily: "HelveticaNeue-Light"
+          fontFamily: "HelveticaNeue-Light",
+          zIndex: 3
         }
       });
 

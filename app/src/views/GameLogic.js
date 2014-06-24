fca5543af9f3c7cbbff116aea23ef75293e1fab4
JoeDou <joe.dou@gmail.com>
2014-06-24 14:47:03 -0700

    update cubesize and perspective

--- app/src/views/GameLogic.js
+++ app/src/views/GameLogic.js
@@ -211,7 +211,7 @@ define(function(require, exports, module) {
     function _determineCubeSize(){
       console.log('windowwidth:', window.innerWidth);
       if (window.innerWidth < 800){
-        this.options.mainCubeSize = 250;
+        this.options.mainCubeSize = 200;
       }
     }
  

a675ce3f6ea3ba1ce1f4b8f94ab088054b8263d9
Amar Patel <theamarpatel@gmail.com>
2014-06-26 11:08:06 -0700

    added dist file, commented out linter--deployment is successful

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('build', [
     'clean:dist',
-    'lint',
+    // 'lint',
     'processhtml:dist',
     'useminPrepare',
     'requirejs',

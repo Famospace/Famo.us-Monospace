d1d223069f66ed9c548b2bff8dff064620debae9
Amar Patel <theamarpatel@gmail.com>
2014-06-30 11:08:46 -0700

    adding cache clean

--- deploy.sh
+++ deploy.sh
@@ -112,6 +112,7 @@ selectNodeVersion
 # 3. Install npm packages
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
+  eval $NPM_CMD cache clean
   eval $NPM_CMD install --production
   exitWithMessageOnError "npm failed"
   cd - > /dev/null

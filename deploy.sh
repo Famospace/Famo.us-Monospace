3e2daa6d9f08aeebac6147a2ea971d76e6db8b24
Amar Patel <theamarpatel@gmail.com>
2014-06-30 12:22:26 -0700

    no grunt

--- deploy.sh
+++ deploy.sh
@@ -128,14 +128,14 @@ if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
 fi
 
 # 5. Run grunt
-if [ -e "$DEPLOYMENT_TARGET/Gruntfile.js" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install grunt-cli
-  exitWithMessageOnError "installing grunt failed"
-  ./node_modules/.bin/grunt --no-color build
-  exitWithMessageOnError "grunt failed"
-  cd - > /dev/null
-fi
+# if [ -e "$DEPLOYMENT_TARGET/Gruntfile.js" ]; then
+#   cd "$DEPLOYMENT_TARGET"
+#   eval $NPM_CMD install grunt-cli
+#   exitWithMessageOnError "installing grunt failed"
+#   ./node_modules/.bin/grunt --no-color build
+#   exitWithMessageOnError "grunt failed"
+#   cd - > /dev/null
+# fi
 ##################################################################################################################################
 
 # Post deployment stub
@@ -146,4 +146,4 @@ if [[ -n "$POST_DEPLOYMENT_ACTION" ]]; then
   exitWithMessageOnError "post deployment action failed"
 fi
 
-echo " Finished successfully."
+echo "Finished successfully."
